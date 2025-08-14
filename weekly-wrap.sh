9eeff4bbd1686a92a6b359044305e718f8399c58
Simon Baird <sbaird@redhat.com>
2025-08-14 17:38:45 -0400

    Tidy up default env var handling

--- weekly-wrap.sh
+++ weekly-wrap.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 set -euo pipefail
 
-if [ -n "${CI:-""}" ]; then
-  # This flag makes claude run silently and quit when it's done
+JIRA_PROJECT="${JIRA_PROJECT:-"EC"}"
+WORD_COUNT="${WORD_COUNT:-"320"}"
+CI="${CI:-""}"
+
+if [ -z "$CI" ]; then
+  PRINT_FLAG=""
+else
+  # This makes claude run silently and quit when it's done
   PRINT_FLAG="--print"
 fi
 
 cat prompt.md |
-  JIRA_PROJECT="${JIRA_PROJECT:-"EC"}" \
-  WORD_COUNT=${WORD_COUNT:-"320"} \
+  JIRA_PROJECT="$JIRA_PROJECT" \
+  WORD_COUNT="$WORD_COUNT" \
   envsubst '$JIRA_PROJECT $WORD_COUNT' |
-  claude ${PRINT_FLAG:-""} --permission-mode acceptEdits
+  claude $PRINT_FLAG --permission-mode acceptEdits
 
 mkdir -p output
 REPORT_TS=output/report-$(date +'%Y%m%d-%H%M%S')
@@ -21,7 +27,7 @@ mv report.md $REPORT_TS.md
 # For convenience let's make an html version
 python -m markdown < $REPORT_TS.md > $REPORT_TS.html
 
-if [ -z "${CI:-""}" ]; then
+if [ -z "$CI" ]; then
   # Take a look
   xdg-open $REPORT_TS.html
 fi
